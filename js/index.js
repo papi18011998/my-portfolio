b9d56bbf771aa28160b31f24df162b99ab3191aa
papi18011998 <papaibrahima98@gmail.com>
2021-11-25 23:39:54 +0000

    feat :boutton back to top

--- js/index.js
+++ js/index.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times')
         $('header').toggleClass('toggle')
     })
+    $(document).scroll(function(){
+        if (document.body.scrollTop > 50  || document.documentElement.scrollTop > 50) {
+            $('.to-top').css("display","block")
+        }else{
+            $('.to-top').css("display","none") 
+        }
+        $('.to-top').click(function(){
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 50
+        })
+    })
 })
